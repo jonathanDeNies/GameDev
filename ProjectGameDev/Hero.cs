8092b58a135364513caa88a4d94fdc40e768b54a
cocovogel <jonathandenies01@gmail.com>
2024-12-06 11:40:01 +0100

    character move methode

--- ProjectGameDev/Hero.cs
+++ ProjectGameDev/Hero.cs
@@ -5,37 +5,48 @@ using ProjectGameDev.interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 
 namespace ProjectGameDev
 {
-    public class Hero:IGameObject
+    public class Hero : IGameObject
     {
 
         Texture2D heroTexture;
-        Animatie animatie;
+        static Animatie animation;
+
+        private Vector2 positie = new Vector2(0, 0);
+        private Vector2 snelheid = new Vector2(1, 1);
+
 
         public Hero(Texture2D texture)
         {
             heroTexture = texture;
-            animatie = new Animatie();
-            animatie.addFrame(new AnimationFrame(new Rectangle(0,0,32,32)));
-            animatie.addFrame(new AnimationFrame(new Rectangle(32, 0, 32, 32)));
-            animatie.addFrame(new AnimationFrame(new Rectangle(64, 0, 32, 32)));
-            animatie.addFrame(new AnimationFrame(new Rectangle(96, 0, 32, 32)));
-            animatie.addFrame(new AnimationFrame(new Rectangle(128, 0, 32, 32)));
+            animation = new Animatie();
+            animation.addFrame(new AnimationFrame(new Rectangle(0, 0, 32, 32)));
+            animation.addFrame(new AnimationFrame(new Rectangle(32, 0, 32, 32)));
+            animation.addFrame(new AnimationFrame(new Rectangle(64, 0, 32, 32)));
+            animation.addFrame(new AnimationFrame(new Rectangle(96, 0, 32, 32)));
+            animation.addFrame(new AnimationFrame(new Rectangle(128, 0, 32, 32)));
         }
 
         public void Update(GameTime gametime)
         {
-            animatie.Update(gametime);
+            animation.Update(gametime);
+            Move();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(heroTexture, new Vector2(0, 0), animatie.CurrectFrame.Rectangle, Color.White);
+            spriteBatch.Draw(heroTexture, positie, animation.CurrectFrame.Rectangle, Color.White);
+        }
+
+        public void Move()
+        {
+            positie += snelheid;
         }
     }
 }
